8b02797f48a1906d79f2b9022c4a86725d09eac2
hemant.kumar1 <hkr5399@gmail.com>
2022-12-04 23:06:21 +0530

    Initial commit

--- Strings/src/anagramSearch.java
+++ Strings/src/anagramSearch.java
@@ -4,20 +4,23 @@ public class anagramSearch {
         String s1 = "geeksforgeeks";
         String s2 = "frog";
         naive(s1,s2);
+        optimize(s1,s2);
     }
 
     private static void naive(String s1, String s2) {
         int m = s1.length(),n=s2.length(),i=0;
         boolean b=false;
         for ( i = 0; i < m-n; i++) {
-            b = checkAnagram(s1,s2,i);
+            if(checkAnagram(s1,s2,i)){
+                System.out.println(i);
+                break;
+            }
         }
-        if(b) System.out.println(i);
     }
 
     private static boolean checkAnagram(String s1, String s2, int i) {
         int[] count = new int[256];
-        for (int j = 0; j <s2.length() ; j++) {
+        for (int j = 0; j <s2.length(); j++) {
             count[s1.charAt(i+j)]++;
             count[s2.charAt(j)]--;
         }
@@ -25,5 +28,30 @@ public class anagramSearch {
             if(count[j]!=0) return false;
         }
         return true;
+    }
+
+    private static void optimize(String s1, String s2) {
+        int[] count1 = new int[256];
+        int[] count2 = new int[256];
+        for (int i = 0; i <s2.length() ; i++) {
+            count1[s1.charAt(i)]++;
+            count2[s2.charAt(i)]++;
+        }
+        for (int i = s2.length(); i < s1.length(); i++) {
+            if(areSame(count1, count2)){
+                System.out.println(i);
+                break;
+            }
+            count1[s1.charAt(i)]++;
+            count1[s1.charAt(i-s2.length())]--;
+        }
+    }
+
+    private static boolean areSame(int[] count1, int[] count2) {
+        boolean flag=true;
+        for (int i = 0; i <256 ; i++) {
+            if(count1[i]!=count2[i]) flag=false;
+        }
+        return flag;
     }
 }

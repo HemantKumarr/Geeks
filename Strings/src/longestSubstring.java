e8f831560ab9b046cd7bb5d66a782b6c86a075f7
hemant.kumar1 <hkr5399@gmail.com>
2022-12-08 09:34:55 +0530

    Initial commit

--- Strings/src/longestSubstring.java
+++ Strings/src/longestSubstring.java
@@ -6,6 +6,8 @@ public class longestSubstring {
 
     public static void main(String[] args){
         String s1 = "abcadbd";
+        best(s1);
+        better(s1);
         int res=0;
         for (int i = 0; i < s1.length(); i++) {
             for (int j = i; j <s1.length() ; j++) {
@@ -14,8 +16,9 @@ public class longestSubstring {
                 }
             }
         }
-        System.out.println(res);
+        System.out.println(res+ " ");
     }
+
 
     private static boolean areDistinct(String s1, int i, int j) {
         boolean[] visited = new boolean[256];
@@ -26,4 +29,37 @@ public class longestSubstring {
         }
         return true;
     }
+
+    private static void better(String s1) {
+        int res=0;
+        for (int i = 0; i < s1.length(); i++) {
+            boolean[] visited = new boolean[128];
+            for (int j = i; j <s1.length() ; j++) {
+                if(visited[s1.charAt(j)])   break;
+                else {
+                    res = max(res, j - i + 1);
+                    visited[s1.charAt(j)]=true;
+                }
+            }
+        }
+        System.out.println(res);
+    }
+
+    private static void best(String s1) {
+        int[] prev = new int[256];
+        Arrays.fill(prev,-1);
+        int res=0;
+        int j=0;
+        for (int i = 0; i <s1.length() ; i++) {
+            if(prev[s1.charAt(i)]==-1){
+                prev[s1.charAt(i)]=i;
+                res++;
+            }
+            j=prev[s1.charAt(i)];
+            res = max(res,i-j+1);
+
+        }
+        System.out.println(res);
+    }
+
 }

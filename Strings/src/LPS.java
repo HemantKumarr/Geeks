e4293b67497131592455e7f346f68a35e5927c72
hemant.kumar1 <hkr5399@gmail.com>
2022-12-11 12:36:44 +0530

    Initial commit

--- Strings/src/LPS.java
+++ Strings/src/LPS.java
@@ -9,6 +9,7 @@ public class LPS {
         for (int i = 0; i <lps.length ; i++) {
             System.out.println(lps[i]);
         }
+        optimize(text);
     }
 
     private static int longPropPreSuff(String text, int n) {
@@ -20,5 +21,29 @@ public class LPS {
             if(flag) return len;
         }
         return 0;
+    }
+
+    private static void optimize(String text) {
+        int[] lps = new int[text.length()];
+        lps[0]=0;
+        int i=1,len=0;
+        while (i<text.length()){
+            if(text.charAt(i)==text.charAt(len)){
+                len++;
+                lps[i]=len;
+                i++;
+            }
+            else
+                if(len==0){
+                    lps[i]=0;
+                    i++;
+                }
+                else{
+                    len=lps[len-1];
+                }
+        }
+        for (int j = 0; j <text.length() ; j++) {
+            System.out.print(lps[j]);
+        }
     }
 }

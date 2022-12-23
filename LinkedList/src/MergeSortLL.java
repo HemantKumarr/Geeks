585b6e89be71c24b66bb9ad49ead4f9a50f128d7
hemant.kumar1 <hkr5399@gmail.com>
2022-12-23 13:02:52 +0530

    Initial commit

--- LinkedList/src/MergeSortLL.java
+++ LinkedList/src/MergeSortLL.java
@@ -4,7 +4,6 @@ public class MergeSortLL {
         Node temp1 = new Node(20);
         Node temp2 = new Node(30);
         Node temp3 = new Node(40);
-Node tempp = ne
         head1.next =temp1;
         temp1.next=temp2;
         temp2.next=temp3;
